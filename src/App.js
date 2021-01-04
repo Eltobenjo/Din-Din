a79f3a1bd7b32ae42725e10a22ac2140e85ce644
Elton Benjamin <eltobenjo@gmail.com>
2021-01-04 15:25:58 +0100

    Header basic

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import Footer from "./components/Footer";
-
+import Header from "./components/Header";
 
 function App() {
   return (
     <div>
+    <Header />
       <h1>Din Din Coming Soon!!</h1>
       <Footer />
     </div>
